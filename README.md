3e7447379ee58aba3f04e56358dd0c6600e89223
mikana0918 <shogo.kusuhara@gmail.com>
2021-04-16 05:37:00 +0900

    feat: add sample API endpoint / introduce docker

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # potfolio-platform-sample
  TBD
+
+# etc(TBD)
+## docker image
+```bash
+docker.io/mikana0918/platform:0.0.1
+```
+## run
+```bash
+docker run -p 8080:8080 -t mikana0918/platform:0.0.1
+```
+
---
+++ src/main/kotlin/com/interfacex/platform/Controllers/WebController.kt
@@ -0,0 +1,14 @@
+package com.interfacex.platform.Controllers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class WebController {
+    @RequestMapping("/")
+    fun index(): String {
+        return "Hello Docker-san!"
+    }
+}
