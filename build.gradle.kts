db29160d8c0ffa7d4f6bab9fe66de7ca03a95d67
mikana0918 <shogo.kusuhara@gmail.com>
2021-04-19 14:24:52 +0900

    feat: :package: add spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
