83b481fe882e30e41559abbb0f0260eca18a59c2
mikana0918 <shogo.kusuhara@gmail.com>
2021-04-19 14:57:34 +0900

    feat: :package: add session storage dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.session:spring-session-data-redis")
+	implementation("org.springframework.session:spring-session-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
